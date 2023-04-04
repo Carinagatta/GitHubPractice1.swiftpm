aec95cd09d0b66e7faef812ad20ca432acb3109c
Claudia <csepko4429@stu.d214.org>
2023-04-04 14:55:08 -0500

    added button, text with answer

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ContentView: View {
     @State var answer = ""
     var body: some View {
-       Button("Multiply", action: <#T##() -> Void#>)
+        Button("Multiply") {
+            
+        }
         Text(answer)
     }
 }
