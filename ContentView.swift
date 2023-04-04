fe9e9e318ae487f2a371ffae98cdb5d672e398fb
Claudia <csepko4429@stu.d214.org>
2023-04-04 14:57:18 -0500

    fixed errors

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,15 @@ import SwiftUI
 struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int = 0
+    @State var answer = ""
     var body: some View {
         VStack {
             TextField("number 1", value: $number1, format: .number)
             TextField("number 2", value: $number2, format: .number)
-        Button("Multiply") {
-            
+            Button("Multiply") {
+                
+            }
+            Text(answer)
         }
-        Text(answer)
     }
 }
