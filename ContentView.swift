0d8cdedd60bb1cf876f7c2163452cd322297c4f9
valeriemorales <vmorales4297@stu.d214.org>
2023-09-21 10:28:27 -0500

    changed the array to type Task, then changed the strings into the variable nameOfTask, then added .nameOfTask property into the list

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks = ["mow the lawn", "feed the dog", "grocery shop"]
+    
+    @State var tasks: [Task] = [
+        Task(nameOfTask: "mow the lawn"),
+        Task(nameOfTask: "feed the dog"),
+        Task(nameOfTask: "grocery shop")
+    ]
     var body: some View {
         VStack {
             List{
                 ForEach(tasks, id: \.self){ task in
-                    Text(task)
+                    Text(task.nameOfTask)
                     
                 }
             }
